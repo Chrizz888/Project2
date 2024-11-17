64a45b0b36d7a0327ee26c56a2014a00e88c858e
Chris Schaefer <chris9869@gmx.de>
2024-11-17 02:08:11 +0000

    Implemented initial JavaScript functions for the Game

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    //Game Variables
+    let userScore = 0;
+    let computerScore = 0;
+
+    //Game Functions
+    function playGame (userChoice) {
+        const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        const computerChoice = choices [Math.floor(Math.random() * choices.length)];
+        //Logic for determing the Winner
+}
+});
